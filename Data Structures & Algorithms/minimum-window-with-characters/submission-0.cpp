ddae6368afee0e6afd56357f36a62295c1bae62d
Narasimhan-257 <83343325+Narasimhan-257@users.noreply.github.com>
2026-07-27 19:22:28 +0530

    Add: minimum-window-with-characters - submission-0

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-0.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    string minWindow(string s, string t) 
+    {
+        int count[128] = {0};
+        int min_l = INT_MAX;
+        int left = 0;
+        int right = 0;
+        int required = 0;
+        int ind = 0;
+        int index = 0;
+        int curr_len = 0;
+        int start = 0;
+        string p = "";
+        for(int i = 0; i < t.length(); i++)
+        {
+            int ind = t[i];
+            count[ind]++;
+            required++; 
+        }
+        while(right < s.length())
+        {
+            index = s[right];
+            
+            if(count[index] > 0)
+            {
+               
+                required--;
+            } 
+            count[index]--;
+            while(required == 0)
+            {
+                curr_len = right - left + 1;
+                if(curr_len < min_l)
+                {
+                    min_l = curr_len;
+                    start = left;
+                }
+
+                ind = s[left];
+                count[ind]++;
+                if(count[ind] > 0)
+                {
+                    required++;
+                }
+                left++;
+            }
+            right++;
+        }
+
+        if(min_l == INT_MAX)
+        {
+            p = "";
+        }
+        else
+        {
+            p = s.substr(start,min_l);
+        }
+        return p;
+    }
+};
